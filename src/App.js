48c0cc6692785b7f8be16bcb23320d111ccb633a
hank0777 <hannahjkifer@gmail.com>
2026-05-26 18:54:33 -0400

    game board, handleClick, winning logic

--- src/App.js
+++ src/App.js
@@ -1,3 +1,78 @@
-export default function Square(){
-  return <button className = "square">X</button>;
+import './App.css';
+import { useState } from 'react';
+
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true); // X is true
+
+  function handleClick(i) {
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  }
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = winner + " wins!";
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
+  return(
+    <>
+      <div className = "status">{status}</div>
+      <div className = "board-row">
+      <Square value = {squares[0]} onSquareClick = {()=>handleClick(0)} />
+      <Square value = {squares[1]} onSquareClick = {()=>handleClick(1)} />
+      <Square value = {squares[2]} onSquareClick = {()=>handleClick(2)} />
+    </div>
+    <div className = "board-row">
+      <Square value = {squares[3]} onSquareClick = {()=>handleClick(3)} />
+      <Square value = {squares[4]} onSquareClick = {()=>handleClick(4)} />
+      <Square value = {squares[5]} onSquareClick = {()=>handleClick(5)} />
+    </div>
+    <div className = "board-row">
+      <Square value = {squares[6]} onSquareClick = {()=>handleClick(6)} />
+      <Square value = {squares[7]} onSquareClick = {()=>handleClick(7)} />
+      <Square value = {squares[8]} onSquareClick = {()=>handleClick(8)} />
+    </div>
+    </>
+  );
+}
+
+function Square({value, onSquareClick}) {
+  return (
+    <button className = "square" onClick = {onSquareClick}>
+      {value}
+    </button>
+  );
+}
+
+function calculateWinner(squares) {
+  const winningRows = [
+    [0, 1, 2],    // horizontal rows
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],    // vertical rows
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],    // diagonals
+    [2, 4, 6]
+  ]
+  for (let i = 0; i < winningRows.length; i++) {
+    const [a, b, c] = winningRows[i];
+    if (squares[a] && squares[a] == squares[b] && squares[a] == squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
