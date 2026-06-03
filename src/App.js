c6863c1e9428500a11d86ee30fe29479891bb91d
hank0777 <hannahjkifer@gmail.com>
2026-06-03 02:02:00 -0400

    Add play against computer and easy mode

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 
 let winningArr = Array(3).fill(null);
 
@@ -11,11 +11,12 @@ function Square({value, onSquareClick, isWinning}) {
   );
 }
 
-function Board({xIsNext, squares, onPlay}) {
-  function handleClick(i) {
-    if (isFullBoard(squares) || calculateWinner(squares) || squares[i]) {
-      return;
-    }
+/* creates game board and checks for winner */
+function Board({xIsNext, squares, onPlay, isOnePlayer, difficultyLevel}) {
+  const winner = calculateWinner(squares);
+  const fullBoard = isFullBoard(squares);
+  
+  const playMove = useCallback((i)=>{
     const nextSquares = squares.slice();
     if (xIsNext) {
       nextSquares[i] = "X";
@@ -23,11 +24,31 @@ function Board({xIsNext, squares, onPlay}) {
       nextSquares[i] = "O";
     }
     onPlay(nextSquares);
+  }, [squares, xIsNext, onPlay]);
+
+  /* two players */
+  function handleClick(i) {
+    if (fullBoard || winner || squares[i]) {
+      return null;
+    }
+    if (isOnePlayer && !xIsNext) {
+      return null;
+    }
+    playMove(i);
   }
 
-  const winner = calculateWinner(squares);
+  /* one player */
+  useEffect(()=>{
+    if (isOnePlayer && !xIsNext && !winner && !fullBoard) {
+      setTimeout(()=>{
+        const move = computerMove(squares, difficultyLevel);
+        playMove(move);
+      }, 1000);
+    }
+  }, [isOnePlayer, xIsNext, winner, fullBoard, squares, difficultyLevel, playMove]);
+
   let status;
-  if (winner === null && isFullBoard(squares)) {
+  if (winner === null && fullBoard) {
     status = "Draw";
   } else if (winner) {
     status = winner + " wins!";
@@ -75,46 +96,54 @@ function LevelButtons({difficultyLevel, onLevelButtonsClick}) {
   );
 }
 
-function MultiPlayer() {
-  const [isOnePlayerClicked, setIsOnePlayerClicked] = useState(true);
-  const [difficultyLevel, setDifficultyLevel] = useState(1);
-
-  function handleMultiPlayerClick(value) {
-    setIsOnePlayerClicked(value);
-  }
-
-  function handleLevelButtonsClick(value) {
-    setDifficultyLevel(value);
-  }
-  
+/* sets number of players and difficulty level */
+function MultiPlayer({isOnePlayerClicked, onMultiPlayerClick, difficultyLevel, onLevelButtonsClick}) {
   if (isOnePlayerClicked) {
     return (
       <>
-        <MultiPlayerButtons isOnePlayerClicked={isOnePlayerClicked} onMultiPlayerClick={handleMultiPlayerClick} />
+        <MultiPlayerButtons isOnePlayerClicked={isOnePlayerClicked} onMultiPlayerClick={onMultiPlayerClick} />
         <div className="level-buttons">
-          <LevelButtons difficultyLevel={difficultyLevel} onLevelButtonsClick={handleLevelButtonsClick} />
+          <LevelButtons difficultyLevel={difficultyLevel} onLevelButtonsClick={onLevelButtonsClick} />
         </div>
       </>
     );
   }
-
   return (
     <>
-      <MultiPlayerButtons isOnePlayer={isOnePlayerClicked} onMultiPlayerClick={handleMultiPlayerClick} />
+      <MultiPlayerButtons isOnePlayerClicked={isOnePlayerClicked} onMultiPlayerClick={onMultiPlayerClick} />
     </>
   );
 }
 
+/* DEFAULT EXPORT FUNCTION */
 export default function Game() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
   const xIsNext = currentMove % 2 === 0;
-  const currentSquares = history[currentMove];
+  const currentSquares = history[currentMove]; /* most updated state of board */
+  const [isOnePlayerClicked, setIsOnePlayerClicked] = useState(true);
+  const [difficultyLevel, setDifficultyLevel] = useState(1);
 
   function handlePlay(nextSquares) {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
     setHistory(nextHistory);
     setCurrentMove(nextHistory.length-1);
+  }
+
+  function handleMultiPlayerClick(value) {
+    if (!isEmptyBoard(currentSquares) && calculateWinner(currentSquares) === null) {
+      return null;
+    }
+    moveTo(0);
+    setIsOnePlayerClicked(value);
+  }
+
+  function handleLevelButtonsClick(value) {
+    if (!isEmptyBoard(currentSquares) && calculateWinner(currentSquares) === null) {
+      return null;
+    }
+    moveTo(0);
+    setDifficultyLevel(value);
   }
 
   function jumpTo(nextMove) {
@@ -123,12 +152,18 @@ export default function Game() {
   }
 
   const moves = history.map((squares, move)=>{
+    /* skips button to navigate to computer's move */
+    if (isOnePlayerClicked && move % 2 === 1) {
+      return null;
+    }
+
     let description;
     if (move > 0) {
       description = "Go to move # " + move;
     } else {
       description = "Go to game start";
     }
+
     return (
       <li key={move}>
         <button onClick={()=>jumpTo(move)}>{description}</button>
@@ -136,7 +171,16 @@ export default function Game() {
     );
   });
 
-  /* -1 is undo, 0 is restart, 1 is redo*/
+  /* skips computer move when undo/redo clicked */
+  let location = 1;
+  const winner = calculateWinner(currentSquares);
+  if (isOnePlayerClicked) {
+    if (winner === null || winner === "O") {
+      location = 2;
+    }
+  }
+
+  /* -1 is undo, 0 is restart, 1 is redo */
   function moveTo(num) {
     if (num === 0) {
       setCurrentMove(0);
@@ -151,15 +195,15 @@ export default function Game() {
     <div className="game">
       <header>{"Tic-Tac-Toe"}</header>
       <div className="select-player-number">
-        <MultiPlayer />
+        <MultiPlayer isOnePlayerClicked={isOnePlayerClicked} onMultiPlayerClick={handleMultiPlayerClick} difficultyLevel={difficultyLevel} onLevelButtonsClick={handleLevelButtonsClick}/>
       </div>
       <div className="move-button">
-        <button onClick={()=>moveTo(0)}>{"Restart"}</button>
-        <button onClick={()=>moveTo(-1)}>{"Undo"}</button>
-        <button onClick={()=>moveTo(1)}>{"Redo"}</button>
+        <button onClick={()=>moveTo(0 * location)}>{"Restart"}</button>
+        <button onClick={()=>moveTo(-1 * location)}>{"Undo"}</button>
+        <button onClick={()=>moveTo(1 * location)}>{"Redo"}</button>
       </div>
       <div className="game-board">
-        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} isOnePlayer={isOnePlayerClicked} difficultyLevel={difficultyLevel} />
       </div>
       <div className="game-info">
         <ol>{moves}</ol>
@@ -168,7 +212,8 @@ export default function Game() {
   );
 }
 
-/* helper functions */
+/* HELPER FUNCTIONS */
+/* finds winning line and updates winningArr */
 function calculateWinner(squares) {
   const winningRows = [
     [0, 1, 2],    // horizontal rows
@@ -190,10 +235,51 @@ function calculateWinner(squares) {
   return null;
 }
 
+/* checks if board is full */
 function isFullBoard(squares) {
   const nullSquares = squares.filter(i => i === null);
   if (nullSquares.length === 0) {
     return true;
   }
   return false;
+}
+
+/* checks if board is empty */
+function isEmptyBoard(squares) {
+  const nullSquares = squares.filter(i => i === null);
+  if (nullSquares.length === squares.length) {
+    return true;
+  }
+  return false;
+}
+
+/* COMPUTER MOVE LOGIC */
+function computerMove(squares, difficultyLevel){
+  /* random move */
+  function makeEasyMove(squares) {
+    let openSquares = [];
+    for (let i = 0; i < squares.length; i++) {
+      if (!squares[i]) {
+        openSquares.push(i);
+      }
+    }
+    let index = Math.floor(Math.random() * openSquares.length);
+    return openSquares[index];
+  }
+
+  function makeMediumMove(squares) {
+
+  }
+
+  function makeHardMove(squares) {
+
+  }
+  
+  if (difficultyLevel === 1) {
+    return makeEasyMove(squares);
+  } else if (difficultyLevel === 2) {
+    return makeMediumMove(squares);
+  } else {
+    return makeHardMove(squares);
+  }
 }
